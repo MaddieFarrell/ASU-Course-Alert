d85873d6673ec0d90fe7fd1c050f72d7360f5158
MaddieFarrell <maddiefarrell7@gmail.com>
2019-04-02 19:58:43 -0700

    Added changes to views

--- waitlist/views.py
+++ waitlist/views.py
@@ -5,6 +5,7 @@ from waitlist.models import StudentPin
 from waitlist.models import Course
 from waitlist.models import WaitingStudent
 from waitlist.models import Semester
+from django.core import serializers
 import requests
 import json
 
@@ -13,52 +14,51 @@ import json
 def sign_up_for_waitlist(request):
 	if request.method == 'POST':
 		json_data == json.loads(request.body)
-		account = Course.StudentPin.objects.create(
+		student_waitlist = Course.StudentPin.objects.create(
 			semester=json_data["semester"],
 			subject_name=json_data["subject_name"],
 			subject_number=json_data["subject_number"],
-			session=json_data["sessionr"],
+			session=json_data["session"],
 			email=json_data["email"],
 			pin=json_data["pin"],
 		)
-		return HttpResponse(pin)
+		return HttpResponse(student_waitlist)
 	return HttpResponse("Error")
 
 @csrf_exempt
 def get_semesters(request):
 	if request.method == 'GET':
-		course_semesters = Course.objects.get(semester)
-		response = "Semester: "
-		for semesters in course_semesters:
-	 		response += " " + semesters.semester + " "
-	return HttpResponse(response)
+		course_semesters = request.GET.get('semester', ' ')
+		qs_json = serializers.serialize('json', Semester.objects.filter(semester=semester))
+	return HttpResponse(qs_json)
 
 @csrf_exempt
 def get_sessions(request):
 	if request.method == 'GET':
-		sessions = Course.objects.get(session)
-		response = "Session: "
-		for sesh in sessions:
-			response += " " + sesh.session + " "
-	return HttpResponse(response)
+		sessions = request.GET.get('session', ' ')
+		qs_json = serializers.serialize('json', Course.objects.filter(session=session))	
+	return HttpResponse(qs_json)
 
 @csrf_exempt
 def get_subject(request):
 	if request.method == 'GET':
-		subjects = Course.objects.get(subject_name, subject_number)
-		response = "Subject: "
-		for subject in subjects:
-			response += " " + subject.subject_name + subject.subject_number + " "
-	return HttpResponse(response)
+		subjects = request.GET.get('subject', ' ')
+		qs_json = serializers.serialize('json', Course.objects.filter(subject_name=subject_name, subject_number=subject_number))	
+	return HttpResponse(qs_json)
 
 @csrf_exempt
 def get_students_waitlist(request):
 	if request.method == 'GET':
-		students_waitlist = Course.objects.all()
-		response = "You're waiting on: "
-		for classes in students_waitlist:
-			response += " " + classes.subject_name + " , " + classes.session + " , " + classes.subject_number + " , " + classes.semester + " "
-	return HttpResponse(response)
+		students_waitlist = WaitingStudent.objects.filter(email=email, course=course)
+		for email in students_waitlist:
+			if email == pin:
+				return HttpResponse(students_waitlist)
+			return("Error")
+
+
+		
+		return HttpResponse(response)
+	return HttpResponse("Please enter your 4 digit pin number")
 
 
 @csrf_exempt
@@ -66,6 +66,11 @@ def pin_recovery(request):
 	if request.method == 'POST':
 		return HttpResponse("Your pin number is %s." % pin)
 #TODO: send pin to the email
+
+
+
+
+
 
 
 
